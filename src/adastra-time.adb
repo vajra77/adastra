b59998bfbbf34032096ad204a11f92515f4a090a
fferreri <f.ferreri@namex.it>
2026-08-11 23:35:15 +0200

    testing OOP style

--- src/adastra-time.adb
+++ src/adastra-time.adb
@@ -1,29 +1,95 @@
 package body Adastra.Time is
 
-    function UT_Fractional_Day (Date: Time_UT) return Time_Fractional_Day is
-        begin
-             return Time_Fractional_Day (Date.Day) +
-                Time_Fractional_Day (Date.Hour) / 24.0 +
-                Time_Fractional_Day (Date.Minutes) / 60.0 +
-                Time_Fractional_Day (Date.Seconds) / 3600.0;
-        end UT_Fractional_Day;
+    ---------------------------------------------------------------------------
+    -- Costruttori e Getter per Time_UT
+    ---------------------------------------------------------------------------
 
-    function UT_Julian_Day (Date: Time_UT) return Time_Julian_Day is
-            Y,M,D,A,B : Precision_Float;
-        begin
-            if Date.Month > 2 then
-                Y := Precision_Float (Date.Year);
-                M := Precision_Float (Date.Month);
-            else
-                Y := Precision_Float (Date.Year - 1);
-                M := Precision_Float (Date.Month + 12);
-            end if;
+    function Make_Time_UT
+      (Year    : Time_Year;
+       Month   : Time_Month;
+       Day     : Time_Day;
+       Hour    : Time_Hour    := 0;
+       Minutes : Time_Minutes := 0;
+       Seconds : Time_Seconds := 0) return Time_UT'Class is
+    begin
+        return Time_UT'(Year    => Year,
+                Month   => Month,
+                Day     => Day,
+                Hour    => Hour,
+                Minutes => Minutes,
+                Seconds => Seconds);
+    end Make_Time_UT;
 
-            D := Precision_Float (Date.Day);
-            A := Precision_Float'Floor(Y / 100.0);
-            B := 2.0 - A + Precision_Float'Floor(A / 4.0);
+    function Year    (Self : Time_UT) return Time_Year    is (Self.Year);
+    function Month   (Self : Time_UT) return Time_Month   is (Self.Month);
+    function Day     (Self : Time_UT) return Time_Day     is (Self.Day);
+    function Hour    (Self : Time_UT) return Time_Hour    is (Self.Hour);
+    function Minutes (Self : Time_UT) return Time_Minutes is (Self.Minutes);
+    function Seconds (Self : Time_UT) return Time_Seconds is (Self.Seconds);
 
-            return Time_Julian_Day (B + 365.25 * Y + 30.6001 * (M + 1.0) + D + 1_720_994.5);
-        end UT_Julian_Day;
+    ---------------------------------------------------------------------------
+    -- Metodi di calcolo per Time_UT
+    ---------------------------------------------------------------------------
+
+    function Fractional_Day (Self : Time_UT) return Time_Fractional_Day is
+    begin
+        return Time_Fractional_Day (Self.Day) +
+               Time_Fractional_Day (Self.Hour) / 24.0 +
+               Time_Fractional_Day (Self.Minutes) / 1440.0 +
+               Time_Fractional_Day (Self.Seconds) / 86400.0;
+    end Fractional_Day;
+
+    function Julian_Day (Self : Time_UT) return Time_Julian_Day is
+        Y, M, D, A, B : Precision_Float;
+    begin
+        if Self.Month > 2 then
+            Y := Precision_Float (Self.Year);
+            M := Precision_Float (Self.Month);
+        else
+            Y := Precision_Float (Self.Year - 1);
+            M := Precision_Float (Self.Month + 12);
+        end if;
+
+        D := Precision_Float (Self.Fractional_Day);
+        A := Precision_Float'Floor (Y / 100.0);
+        B := 2.0 - A + Precision_Float'Floor (A / 4.0);
+
+        return Time_Julian_Day (B + Precision_Float'Floor (365.25 * Y) +
+               Precision_Float'Floor (30.6001 * (M + 1.0)) + D + 1_720_994.5);
+    end Julian_Day;
+
+    ---------------------------------------------------------------------------
+    -- Costruttori, Getter e Metodi per Time_LCT
+    ---------------------------------------------------------------------------
+
+    function Make_Time_LCT
+      (Year    : Time_Year;
+       Month   : Time_Month;
+       Day     : Time_Day;
+       Hour    : Time_Hour    := 0;
+       Minutes : Time_Minutes := 0;
+       Seconds : Time_Seconds := 0;
+       Zone    : Time_Zone    := 0) return Time_UT'Class is
+    begin
+        return Time_LCT'(Year    => Year,
+                Month   => Month,
+                Day     => Day,
+                Hour    => Hour,
+                Minutes => Minutes,
+                Seconds => Seconds,
+                Zone    => Zone);
+    end Make_Time_LCT;
+
+    function Zone (Self : Time_LCT) return Time_Zone is (Self.Zone);
+
+    overriding
+    function Fractional_Day (Self : Time_LCT) return Time_Fractional_Day is
+        Hours_In_UT : Standard.Float := Standard.Float (Self.Hour) - Standard.Float (Self.Zone);
+    begin
+        return Time_Fractional_Day (Self.Day) +
+               Time_Fractional_Day (Hours_In_UT) / 24.0 +
+               Time_Fractional_Day (Self.Minutes) / 1440.0 +
+               Time_Fractional_Day (Self.Seconds) / 86400.0;
+    end Fractional_Day;
 
 end Adastra.Time;
--- src/adastra-time.ads
+++ src/adastra-time.ads
@@ -1,40 +1,82 @@
-with Ada.Calendar;
 with Adastra.Types; use Adastra.Types;
 
 package Adastra.Time is
 
-    type Months is (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec);
-    type Time_Year is new Positive range 1977 .. 2050;
-    type Time_Month is new Positive range 1 .. 12;
-    type Time_Day is new Positive range 1 .. 31;
-    type Time_Hour is new Natural range 0 .. 23;
+    type Time_Year    is new Positive range 1977 .. 2050;
+    type Time_Month   is new Positive range 1 .. 12;
+    type Time_Day     is new Positive range 1 .. 31;
+    type Time_Hour    is new Natural range 0 .. 23;
     type Time_Minutes is new Natural range 0 .. 59;
     type Time_Seconds is new Natural range 0 .. 59;
 
     type Time_Zone is new Integer range -12 .. 12;
 
     type Time_Fractional_Day is new Precision_Float;
-    type Time_Julian_Day is new Precision_Float;
+    type Time_Julian_Day     is new Precision_Float;
 
-    type Time_UT is record
-        Year : Time_Year;
-        Month : Time_Month;
-        Day :  Time_Day;
-        Hour : Time_Hour;
-        Minutes : Time_Minutes;
-        Seconds : Time_Minutes;
-    end record;
+    ---------------------------------------------------------------------------
+    -- Classe Base Privata: Time_UT
+    ---------------------------------------------------------------------------
+    type Time_UT is tagged private;
 
-    type Time_LCT is record
-        Zone : Time_Zone;
-        Year : Time_Year;
-        Month : Time_Month;
-        Day :  Time_Day;
-        Hour : Time_Hour;
+    -- Costruttore per Time_UT
+    function Make_Time_UT
+      (Year    : Time_Year;
+       Month   : Time_Month;
+       Day     : Time_Day;
+       Hour    : Time_Hour    := 0;
+       Minutes : Time_Minutes := 0;
+       Seconds : Time_Seconds := 0) return Time_UT'Class;
+
+    -- Getter (interfaccia pubblica)
+    function Year    (Self : Time_UT) return Time_Year;
+    function Month   (Self : Time_UT) return Time_Month;
+    function Day     (Self : Time_UT) return Time_Day;
+    function Hour    (Self : Time_UT) return Time_Hour;
+    function Minutes (Self : Time_UT) return Time_Minutes;
+    function Seconds (Self : Time_UT) return Time_Seconds;
+
+    -- Metodi principali
+    function Fractional_Day (Self : Time_UT) return Time_Fractional_Day;
+    function Julian_Day     (Self : Time_UT) return Time_Julian_Day;
+
+    ---------------------------------------------------------------------------
+    -- Classe Derivata Privata: Time_LCT
+    ---------------------------------------------------------------------------
+    type Time_LCT is new Time_UT with private;
+
+    -- Costruttore per Time_LCT
+    function Make_Time_LCT
+      (Year    : Time_Year;
+       Month   : Time_Month;
+       Day     : Time_Day;
+       Hour    : Time_Hour    := 0;
+       Minutes : Time_Minutes := 0;
+       Seconds : Time_Seconds := 0;
+       Zone    : Time_Zone    := 0) return Time_UT'Class;
+
+    -- Getter aggiuntivo per LCT
+    function Zone (Self : Time_LCT) return Time_Zone;
+
+    -- Override del metodo Fractional_Day
+    overriding
+    function Fractional_Day (Self : Time_LCT) return Time_Fractional_Day;
+
+private
+
+    -- Definizione reale della struttura di Time_UT
+    type Time_UT is tagged record
+        Year    : Time_Year;
+        Month   : Time_Month;
+        Day     : Time_Day;
+        Hour    : Time_Hour;
         Minutes : Time_Minutes;
         Seconds : Time_Seconds;
     end record;
 
-    function UT_Fractional_Day (Date: Time_UT) return Time_Fractional_Day;
-    function UT_Julian_Day (Date: Time_UT) return Time_Julian_Day;
+    -- Definizione reale della struttura di Time_LCT
+    type Time_LCT is new Time_UT with record
+        Zone : Time_Zone := 0;
+    end record;
+
 end Adastra.Time;
